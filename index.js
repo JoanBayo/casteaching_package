fb709d5311949db15466276c3769cd5d03d2380f
Bayo <jbayo@iesebre.com>
2023-03-09 15:19:43 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,48 @@
+import axios from 'axios'
+
+// TOKEN 4dtQl58nAQ6ciWW04BHdQCav2WbMnspuJnHjGYrO
+const apiClinet = axios.create({
+    baseURL: 'http://casteachingbayo.test/api',
+    withCredentials : true,
+    headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer 4dtQl58nAQ6ciWW04BHdQCav2WbMnspuJnHjGYrO'
+    }
+})
+
+
+
+export default {
+    videos: async function() {
+        const response = await apiClinet.get(' /videos')
+        return response.data
+    },
+    login:  async function(email,password,device_name) {
+        const postData = {
+            email,
+            password,
+            device_name,
+        }
+        const response = await apiClient.post('/sanctum/token', postData)
+        return response.data
+    },
+    video: {
+        show: async function(id) {
+            const response = await apiClinet.get(' /videos/' + id)
+            return response.data
+        },
+        create: async function(data) {
+            const response = await apiClinet.post(' /videos',data)
+            return response.data
+        },
+        update: async function(id, data) {
+            const response = await apiClinet.put(' /videos/'+ id,data)
+            return response.data
+        },
+        destroy: async function(id) {
+            const response = await apiClinet.delete(' /videos/' + id)
+            return response.data
+        },
+    }
+}
